e2100fb7a23d7f7b96a0c742b5052a295f0221d6
Matt Brundage II <mattjbrundage@icloud.com>
2021-05-03 16:05:07 -0400

    More Code

--- PriceArray.cpp
+++ PriceArray.cpp
@@ -1,6 +1,7 @@
 //Matt Brundage II
 //4/22/21
-//Increases prices in array/displays increased prices
+//PriceArray.cpp
+//Increases prices in Array/Displays increased prices
 
 #include <iostream>
 #include <iomanip>
@@ -10,8 +11,18 @@ int main()
 {
 	cout << fixed << setprecision(2);
 
-	//declare array
+	//Declare Array
 	double prices[10] = {4.5, 6.75, 23.0, 21.5, 5.25, 8.99, 9.99, 10.89, 3.99, 4.0};
+
+	//Declare Variables
+	double percentage = 0.0;
+
+	//Accept User Input
+	cout << "By what percentage do you wish to increase prices? ";
+	cin >> percentage;
+	percentage <= 100;
+
+	//Increase the Prices
 
 	return 0;
 }	//end of main function
